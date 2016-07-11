792382284a14af7fcee8904815e7b5cfda640c1a
git <loic@orbix.fr>
2016-07-11 22:35:02 +0200

    Test commit

--- ext_localconf.php
+++ ext_localconf.php
@@ -17,6 +17,7 @@ if (!defined('TYPO3_MODE')) {
 	)
 );
 
+## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
+
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['messageList'] = 'EXT:'.$_EXTKEY.'/class.eid_message_list.php';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['messageCreate'] = 'EXT:'.$_EXTKEY.'/class.eid_message_create.php';
-
